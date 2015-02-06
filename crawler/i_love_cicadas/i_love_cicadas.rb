c83a2b67dcf27ce0d899de3c0dfe56e6232df506
cigalecigales <cigalecigales@gmail.com>
2015-02-06 23:06:52 +0900

    Ver1.2.3 Finish i_love_cicadas.rb

--- crawler/i_love_cicadas/i_love_cicadas.rb
+++ crawler/i_love_cicadas/i_love_cicadas.rb
@@ -1,5 +1,8 @@
 require 'flickraw'
 require 'open-uri'
+require 'google/api_client'
+require 'google_drive'
+require 'fileutils'
 
 #
 # 画像取得用クラス
@@ -11,13 +14,18 @@ class FlickrSearch
   # 画像を検索する
   def search_img(word, per_page, sort, upload_date)
     images = flickr.photos.search :tags => word, :sort => sort, :per_page => per_page, :min_upload_date => upload_date
+		return images
   end
 
-  # 画像を保存する
-  def save_img(images = [])
+	# ディレクトリを作成する
+	def create_dir
     dir_name = "今日のセミ" + "(" + Time.now.strftime('%Y年%m月%d日%H時%M分%S秒') + ")"
-    Dir::mkdir(dir_name) unless File.exist?(dir_name)
+    FileUtils.mkdir(dir_name) unless File.exist?(dir_name)
+		return dir_name
+	end
 
+  # 画像を保存する
+  def save_img(images = [], dir_name)
     images.each do |image|
       s_url = FlickRaw.url image
       file_name = File.basename(s_url)
@@ -36,16 +44,65 @@ end
 # 画像アップロード用クラス
 #
 class GoogleDriveUpload
+	CLIENT_ID = ''
+	CLIENT_SECRET = ''
+	REFRESH_TOKEN = ''
+	
+	def upload_dir(dir_name, images = [])
+		client = OAuth2::Client.new( 
+			CLIENT_ID, CLIENT_SECRET,
+			:site => "https://accounts.google.com",
+			:token_url => "/o/oauth2/token",
+			:authorize_url => "/o/oauth2/auth"
+		)
 
+		auth_token = OAuth2::AccessToken.from_hash(client, { :refresh_token => REFRESH_TOKEN, :expire_at => 3600 })
+		auth_token = 	auth_token.refresh!
+
+		session = GoogleDrive.login_with_oauth(auth_token.token)
+
+		@folder = session.root_collection.create_subcollection(dir_name)
+
+		dir = Dir.open(dir_name)
+		dir.each do |image|
+			if !(/^\.+$/ =~ image.to_s)
+				puts image.to_s
+				file = session.upload_from_file(dir_name + "/" + image.to_s, image.to_s, :convert => false)
+				@folder.add(file)
+				session.root_collection.remove(file)
+			end
+		end
+		dir.close
+	end
 end
 
 #####################################################################################################################
-word = "cicada" # 検索ワード
+word = ["cicada", "蝉", "セミ"] # 検索ワード
 per_page = 100 # 画像取得数
 sort = "date-posted-desc" # ソート順
 upload_date = Time.now - 3*24*60*60 # 基準日
 
+puts "検索ワード:" + word.map{ |tag| "#{ tag }" }.join(", ")
+puts "基準日:" + upload_date.to_s
+
+puts "******************* 画像取得スタート **********************"
 flickr_obj = FlickrSearch.new
 images = flickr_obj.search_img(word, per_page, sort, upload_date)
-flickr_obj.save_img(images)
+dir_name = flickr_obj.create_dir
+
+puts "ディレクトリ名:" + dir_name
+puts "画像数:" + images.size.to_s
+
+if images.size != 0
+	flickr_obj.save_img(images, dir_name)
+	puts "********************* 画像取得完了*************************"
+
+	puts "**************** 画像アップロードスタート *****************"
+	google_obj = GoogleDriveUpload.new
+	google_obj.upload_dir(dir_name)
+	puts "****************** 画像アップロード完了 *******************"
+end
+
+FileUtils.rm_r(dir_name)
+puts "****************** ディレクトリ削除完了 *******************"
 #####################################################################################################################
