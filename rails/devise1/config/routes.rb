202f589e44a80fbd624937ea126411096d743302
cigalecigales <cigalecigales@gmail.com>
2015-02-11 10:48:29 +0900

    Ver1.3.2 Finish devise settings

--- rails/devise1/config/routes.rb
+++ rails/devise1/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
+  devise_for :users
+	root 'home#index'
   get 'home/show'
 
   # The priority is based upon order of creation: first created -> highest priority.
