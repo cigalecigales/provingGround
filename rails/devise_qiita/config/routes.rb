21a9966bb5780aa2f5b9616504719132c740fef3
cigalecigales <cigalecigales@gmail.com>
2015-02-17 23:16:53 +0900

    Ver1.3.9 Add devise views

--- rails/devise_qiita/config/routes.rb
+++ rails/devise_qiita/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  get 'pages/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
