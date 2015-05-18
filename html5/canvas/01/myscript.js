db10d5d0ef2a035ecc7335fffe08329b5950f50f
cigalecigales <cigalecigales@gmail.com>
2015-05-18 22:49:24 +0900

    Ver1.8.6 Add html5 canvas

---
+++ html5/canvas/01/myscript.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+	draw();
+}
+
+function draw(){
+	var canvas = document.getElementById('mycanvas');
+	if(!canvas || !canvas.getContext) return false;
+	var ctx = canvas.getContext('2d');
+
+	//ctx.strokeStyle = "#00ff00";
+	//ctx.fillStyle = "#ff0000";
+	//ctx.lineWidth = 6;
+	//ctx.lineJoin = "round"; //bevel, miter
+
+	//ctx.strokeRect(10,10,50,50);
+	//ctx.fillRect(10,80,50,50);
+	//ctx.clearRect(15,15,40,40);
+	
+	//var g = ctx.createLinearGradient(0,0,100,100);
+	var g = ctx.createRadialGradient(50,50,20,50,50,60);
+	g.addColorStop(0.0, "red");
+	g.addColorStop(0.5, "blue");
+	g.addColorStop(1.0, "green");
+	ctx.fillStyle = g;
+	ctx.fillRect(0,0,100,100);
+}
---
+++ javascript/02/main.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+}
+
+function swithDisabled(el, className){
+	
+}
