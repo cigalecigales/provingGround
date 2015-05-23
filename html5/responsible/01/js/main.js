61ecb6e9b73dd622075e2d0ada6c8f73ccfd8f09
cigalecigales <cigalecigales@gmail.com>
2015-05-24 00:11:36 +0900

    Ver1.9.3 load json file and create views

---
+++ html5/responsible/01/js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+  $("#gallery_items").each(function(){
+    var $container = $(this);
+
+    $.getJSON('./json/contents.json', function(data){
+      var elements = [];
+
+      $.each(data, function(i, item){
+        var itemHTML =
+                  '<li class="gallery_item">' +
+                    '<div class="gallery_image_base">' +
+                      '<img src="' + item.image + '" class="gallery_image">' +
+                    '</div>'
+                  '</li>';
+        elements.push($(itemHTML).get(0));
+      });
+
+      $container.append(elements);
+
+    });
+  });
+});
