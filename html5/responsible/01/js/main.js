21a230449cf58c8ec99a60c7126bda6567636c3b
cigalecigales <cigalecigales@gmail.com>
2015-05-24 01:51:18 +0900

    Ver1.9.4 Add load button function

--- html5/responsible/01/js/main.js
+++ html5/responsible/01/js/main.js
@@ -1,11 +1,26 @@
 $(function(){
   $("#gallery_items").each(function(){
-    var $container = $(this);
+    var $container = $(this),
+        $load_button = $("#load_button"),
+        add_item_count = 3,
+        added_items = 0,
+        all_data = [],
+        add_data = [];
 
-    $.getJSON('./json/contents.json', function(data){
-      var elements = [];
+    $.getJSON('./json/contents.json', init);
 
-      $.each(data, function(i, item){
+    function init(data){
+      all_data = data;
+      add_items();
+      $load_button.on("click", add_items);
+    }
+
+    function add_items(){
+      var elements = [],
+          slice_data = all_data.slice(added_items, added_items + add_item_count),
+          scroll_position;
+
+      $.each(slice_data, function(i, item){
         var itemHTML =
                   '<li class="gallery_item">' +
                     '<div class="gallery_image_base">' +
@@ -14,9 +29,23 @@ $(function(){
                   '</li>';
         elements.push($(itemHTML).get(0));
       });
-
       $container.append(elements);
 
-    });
+      added_items += slice_data.length;
+
+      check_added_all();
+
+      $('html,body').animate({ scrollTop: 0 }, '1');
+    }
+
+    function check_added_all(){
+      if(added_items == all_data.length){
+        $load_button.css("display", "none");
+      }
+    }
+
+    function set_scroll_position(){
+
+    }
   });
 });
