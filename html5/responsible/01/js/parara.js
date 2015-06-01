ca995df09f4c9edc5313e1575457a5a87fb4e876
cigalecigales <cigalecigales@gmail.com>
2015-06-01 23:35:46 +0900

    Ver2.1.2 Fix parallax

--- html5/responsible/01/js/parara.js
+++ html5/responsible/01/js/parara.js
@@ -1,9 +1,9 @@
 $(function(){
-  var top = $("#sq1").offset().top;
+  var top = $("#circle1").offset().top;
   $(window).scroll(function(){
     var value = $(this).scrollTop();
-    $("#sq1").css("top", top + value / 2);
-    $("#sq2").css("top", top + value / 4);
-    $("#sq3").css("top", top + value / 6);
+    $("#circle1").css("top", top - value);
+    $("#circle2").css("top", top - value / 4);
+    $("#circle3").css("top", top - value / 6);
   });
 });
