0d3484f99ddb1e28542f764130c30365bca868ee
cigalecigales <cigalecigales@gmail.com>
2015-06-03 23:16:31 +0900

    Ver2.1.4 Add navigator files

---
+++ javascript/08/navigator.js
@@ -0,0 +1,11 @@
+var navigator = document.navigator;
+
+//for(var c in navigator){
+//	document.write(navigator[c] + "<br/>");
+//}
+
+display(navigator.appCodeName);
+
+function display(value){
+	document.write(value + "<br/>");
+}
